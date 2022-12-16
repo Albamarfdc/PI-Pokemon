7b717d99e024236c0578cc44c2bdffe2aab0f0b8
Albamarfdc <albamarf3@gmail.com>
2022-12-16 17:40:42 -0400

    deploy

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -10,7 +10,7 @@ export const ORDER_BY_NAME = "ORDER_BY_NAME";
 export const ORDER_BY_ATTACK = "ORDER_BY_ATTACK";
 export const DELETE_POKEMON = "DELETE_POKEMON";
 
-const BACK_URL = "http://localhost:3001";
+const BACK_URL = "https://pokeapp-6ds1.onrender.com";
 
 export function getPokemons() {
   return async function (dispatch) {
