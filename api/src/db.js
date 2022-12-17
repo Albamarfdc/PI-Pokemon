b3a2ce87aab88e58e4e72691ce167e3538459f00
Albamar Flores <89668743+Albamarfdc@users.noreply.github.com>
2022-12-17 14:35:15 -0400

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {PGUSER,PGPASSWORD,PGHOST,PGPORT,PGDATABASE } = process.env;
+const {DATABASE_URL} = process.env;
 
 
 
-const sequelize = new Sequelize(`postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`, {
+const sequelize = new Sequelize(`${DATABASE_URL}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectModule: require('pg'),
