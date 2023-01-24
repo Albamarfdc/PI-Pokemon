b66bb9c27b4839e726ab66fbed2d1bcecb3a334c
Albamar Flores <89668743+Albamarfdc@users.noreply.github.com>
2023-01-23 20:08:25 -0400

    Update index.js

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -10,7 +10,7 @@ export const ORDER_BY_NAME = "ORDER_BY_NAME";
 export const ORDER_BY_ATTACK = "ORDER_BY_ATTACK";
 export const DELETE_POKEMON = "DELETE_POKEMON";
 
-const BACK_URL = "https://pokeapp-6ds1.onrender.com";
+const BACK_URL = "https://pokeapp.fly.dev";
 
 export function getPokemons() {
   return async function (dispatch) {
