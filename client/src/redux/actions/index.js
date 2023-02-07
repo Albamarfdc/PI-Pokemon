59f7899303d2e7b4e8bd809e07944969a68eb7d4
Albamar Flores <89668743+Albamarfdc@users.noreply.github.com>
2023-02-07 12:45:56 -0400

    Update index.js

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -10,7 +10,7 @@ export const ORDER_BY_NAME = "ORDER_BY_NAME";
 export const ORDER_BY_ATTACK = "ORDER_BY_ATTACK";
 export const DELETE_POKEMON = "DELETE_POKEMON";
 
-const BACK_URL = "https://backend-hpta.onrender.com/pokemons";
+const BACK_URL = "https://backend-hpta.onrender.com";
 
 export function getPokemons() {
   return async function (dispatch) {
